c6647d83710615ac29d2248b70af6e9d7bb7baee
Christoph Steefel <cfsteefel@gmail.com>
2015-10-03 14:07:09 -0700

    Changed movement script, speeds up after 5 seconds.

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	//Base speed
+	public float baseSpeed = .5f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		// Move in the direction of the camera.
+		transform.position = transform.position + Camera.main.transform.forward * baseSpeed * Time.deltaTime;
+		if (Time.time > 5) {
+			// Update the speed after 5 seconds
+			baseSpeed = 5;
+		}
+	}
+
+}
