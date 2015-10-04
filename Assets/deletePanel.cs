5277c28a882611786eb3a68ef380a509c41cff7b
Christoph Steefel <cfsteefel@gmail.com>
2015-10-03 17:25:56 -0700

    Adding text deletion version

---
+++ Assets/deletePanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deletePanel : MonoBehaviour {
+	public int delay = 5;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
