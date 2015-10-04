eda68a2f220288d80eb2c5e187d4abca179126ec
Byron Jay Inocencio <binocenc@ucsd.edu>
2015-10-04 10:23:38 -0700

    added jelly sound

--- Assets/ActualDestroyer.cs
+++ Assets/ActualDestroyer.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class ActualDestroyer : MonoBehaviour {
-
+	
 	int jellyCount = 0;
 	public delegate void LevelAction();
 	public static event LevelAction levelUp;
 	public int DELAY = 3;
-
-
-
+	
+	private AudioSource audioSource;
+	
+	[SerializeField]
+	public AudioClip deathSound;
+	
+	void Start () {
+		
+		audioSource = GetComponent<AudioSource> ();
+		
+	}
+	
 	void OnTriggerEnter (Collider other)
 	{
 		if(other != null && other.tag == "monster")
@@ -20,8 +29,11 @@ public class ActualDestroyer : MonoBehaviour {
 				levelUp();
 			}
 			
+			transform.LookAt (Camera.main.transform);
+			audioSource.PlayOneShot(deathSound);
 		}
 	}
-
-
+	
+	void OnTriggerExit() {
+	}
 }
