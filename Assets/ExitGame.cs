9611a1557ae9cc1589726a82cc489ecea7542082
Christoph Steefel <cfsteefel@gmail.com>
2015-10-04 02:31:25 -0700

    Added exit game on tilt and pause game on button press

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour {
+
+	void OnEnable() {
+		Cardboard.SDK.OnTrigger += PauseGame;
+		Cardboard.SDK.OnTilt += CloseGame;
+	}
+	void OnDisable() {
+		Cardboard.SDK.OnTrigger -= PauseGame;
+		Cardboard.SDK.OnTilt -= CloseGame;
+	}
+	
+	void CloseGame() {
+		Application.Quit ();
+	}
+
+	void PauseGame() {
+		GetComponent<MovementScript>().moving = !GetComponent<MovementScript>().moving;
+	}
+}
